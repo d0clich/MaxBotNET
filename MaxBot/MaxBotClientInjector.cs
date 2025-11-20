cfbb48a21a8ee482dc37f326479663b20f547133
dl1ch <dl1ch@DESKTOP-VUCVL9A>
2025-11-20 14:22:47 +0300

    Injector

---
+++ MaxBot/MaxBotClientInjector.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+namespace MaxBot
+{
+    public static class MaxBotClientInjector
+    {
+        public static IServiceCollection AddMaxBotClient(this IServiceCollection services, string token)
+        {
+            services.AddHttpClient<MaxBotClient>(x=>new MaxBotClient(token, x));
+            return services;
+        }
+    }
+}
